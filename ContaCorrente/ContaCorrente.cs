057699e69a3e1eba83f8ba97deeb054dcac24456
Alisson <Alisson@DESKTOP-V8P831N>
2023-04-09 15:54:00 -0300

    Add project files.

---
+++ ContaCorrente/ContaCorrente.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrente
+{
+
+    internal class ContaCorrente
+    {
+        public string nome;
+        public int operacao;
+        public int movimentacoes;        
+        public int numeroDaConta = 0;
+        public int saldo = 0;
+        public bool ehEspecial = true;
+        public int limite = 0;
+        ArrayList movimentacoesEntreContas = new ArrayList();
+        private string movOperacao;
+
+        public ContaCorrente(string nome , int saldo , int limite , bool ehEspecial , int numeroDaConta)
+        {
+            this.nome = nome;
+            this.saldo = saldo;
+            this.limite = limite;
+            this.numeroDaConta = numeroDaConta;
+            this.ehEspecial = ehEspecial;
+        }
+
+        public ContaCorrente()
+        {
+        }
+
+        public void Movimentações(int quantidade, string nomeOperacao)
+       
+        {
+            ContaCorrente movimentacao = new ContaCorrente();
+            {
+                movOperacao = nomeOperacao + " " + quantidade;
+
+            }
+
+            movimentacoesEntreContas.Add(movimentacao.movOperacao);
+
+
+        }
+             
+        public void Deposito(int valor)
+        {
+            saldo += valor;
+            Movimentações(valor, "Deposito + ");
+        }
+
+        public void Saque(int valor)
+        {
+            if (saldo > valor)
+                return;
+            saldo -= valor;
+
+
+            Movimentações(valor, "Saque - ");
+        }
+
+        public int VisualizarSaldo()
+        {
+            return this.saldo;
+        }
+
+        public void Tranferencia(int valor, ContaCorrente valorEntrada)
+        {
+            if (saldo > valor)
+                return;
+
+            saldo -= valor;
+
+            valorEntrada.saldo += valor;
+
+            Movimentações(valor, "Tranferência - ");
+
+            valorEntrada.Movimentações(valor, "Tranferência + ");
+
+        }
+
+        public void ExibirExtrato()
+        {
+            foreach (var item in movimentacoesEntreContas)
+            {
+                Console.WriteLine(item);
+            }
+        
+        } 
+
+    }
+}
---
+++ ContaCorrente/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace ContaCorrente
+{
+
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta1 = new ContaCorrente("Alisson Scopel", 2000, 2500, false , 1);
+            var conta2 = new ContaCorrente("Gustavo Ramos", 1000 , 1500, true , 2);
+
+            conta1.Saque(500);
+
+            conta1.Deposito(300);          
+
+            conta1.Saque(200);
+
+            conta2.Tranferencia(200, conta1);
+
+            conta2.Saque(500);
+
+            Console.WriteLine("Conta 2 Extrato:\n");
+            Console.WriteLine("Valor inicial 1000");
+            conta2.ExibirExtrato();
+            Console.WriteLine("Valor Final: " + conta2.VisualizarSaldo());
+
+            Console.WriteLine("-----------------------------------------------------------------------------\n");
+            
+            Console.WriteLine("Conta 1 Extrato: \n");
+            Console.WriteLine("Valor inicial 2000");
+            conta1.ExibirExtrato();
+            Console.WriteLine("Valor Final: " + conta1.VisualizarSaldo());
+        }
+    }
+}
